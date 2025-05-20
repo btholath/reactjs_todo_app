d6823344d19964d405e375c7143ffe84d316d83e
Biju Tholath <54602395+btholath@users.noreply.github.com>
2025-05-20 21:31:21 +0000

    version 1.2

--- todo-list-app/src/main.jsx
+++ todo-list-app/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {configureStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
+
 import App from './App.jsx'
+
+// In-memory database store
+const store = configureStore({
+  reducer: {},
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>,
 )
